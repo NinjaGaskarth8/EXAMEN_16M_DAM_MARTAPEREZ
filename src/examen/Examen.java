1778e97edd1ad2f5494df587fb6300a6d8b1cb14
Marta <Marta@DESKTOP-KGQQT70>
2022-05-16 09:22:58 +0200

    Comentario master

---
+++ src/examen/Examen.java
@@ -0,0 +1,19 @@
+package examen;
+
+import java.util.Scanner;
+
+public class Examen {
+
+	public static void main(String[] args) {
+		
+		Scanner ent = new Scanner (System.in);
+		int numero;
+		
+		System.out.println("Buenos días Ceinmark, soy Marta Pérez");
+		
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+		numero = ent.nextInt();
+
+	}
+
+}
